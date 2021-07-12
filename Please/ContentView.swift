4ec824ddf847d307eef4bb97a87cbf2af953befb
Donguk Lee <ldu2175@gmail.com>
2021-07-12 20:17:03 +0900

    init

---
+++ Please/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Please
+//
+//  Created by 이동욱 on 2021/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Please/PleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseApp.swift
+//  Please
+//
+//  Created by 이동욱 on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct PleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
